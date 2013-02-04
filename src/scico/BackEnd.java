7dd11fb4c20e97231150b7aba7346bbeecac9a83
James Rothering <jr@jamesrothering.com>
2013-02-04 10:32:01 -0800

    Getting the hang of egit. This is an attempt to commit and push.

--- src/scico/BackEnd.java
+++ src/scico/BackEnd.java
@@ -4,7 +4,8 @@ public class BackEnd
 {
   public static void main(String[] argv)
   {
- // This is github testing code, to see if I can push from Egit in Eclipse   
+    //This is github testing code, to see if I can push from Egit in Eclipse 
+    //this is a minor change
     System.out.println("hello world!");
   }
 }

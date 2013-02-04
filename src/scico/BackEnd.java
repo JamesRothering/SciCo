15b8bedf57a14832cf96545e1b113575f4e95888
jr <jrothering@choozy.me>
2013-02-04 10:28:24 -0800

    This is a first commit to get the environment set up correctly, and ensure that Egit is working correctly.

---
+++ src/scico/BackEnd.java
@@ -0,0 +1,10 @@
+package scico;
+
+public class BackEnd
+{
+  public static void main(String[] argv)
+  {
+ // This is github testing code, to see if I can push from Egit in Eclipse   
+    System.out.println("hello world!");
+  }
+}

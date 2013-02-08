12740a67ccbcff355cc88307ddd4a2a285caebce
JamesRothering <jr@jamesrothering.com>
2013-02-08 00:23:09 -0800

    working out the kinks

---
+++ src/com/scico/BackEnd.java
@@ -0,0 +1,14 @@
+package com.scico;
+
+public class BackEnd {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("gothere");
+		System.out.println("also did this");
+
+	}
+
+}
